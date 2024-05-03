34cae0b9351d0c984e1376dd265cd52523f27d84
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 15:35:44 -0500

    Add files via upload

---
+++ JAVA1.java
@@ -0,0 +1,9 @@
+package com.mycompany.java1;
+
+public class JAVA1 {
+
+     public static void main(String[] args) {
+        System.out.println(10/5+6-2+3*8*1-12/2+9-7*4);
+    }
+}
+//Es JAVA I, ejercicio 5
